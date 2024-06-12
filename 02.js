3b68941cb8a005d6e967d227c9e4a28581bfb378
Sanchita Jedhe <sanchitajedhe@gmail.com>
2024-06-12 18:10:58 +0530

    data type summary

--- 02.js
+++ 02.js
@@ -1,1 +1,42 @@
-console.log("hitesh")
+console.log("hitesh");
+console.log("2"===2);
+console.log(undefined == 0);
+console.log("--------------------");
+//primitive data type
+//string,number,boolean,null,undefined,BigInt,Symbol
+
+//refrence type - non primitive 
+//Array,Objects,function
+//javascript is dyanamic type bcz in variable we canstore diff type of data  
+//we dont need to define data type
+
+const isLoggedIn =false
+const score=100;
+let userEmail;
+
+const id=Symbol('123');
+const anotherid=Symbol('123')
+
+console.log(id == anotherid); //false
+
+//Arrays 
+const color=["pink","purple","blue"];
+
+//objets
+let myObj= {
+    name:"anu", //objet are in key value pair
+    age:18,
+
+}
+console.log(myObj);
+
+//function
+const myfun =function(){
+    console.log("hello world");
+}
+console.log(typeof myfun ); // function
+console.log(typeof color); //objets
+console.log(typeof myobj); //undefined
+
+
+
