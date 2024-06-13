8b48f6c4a55dffce07bad11b899df1c5581e8d2a
Sanchita  Jedhe <144338706+SanchitaJedhe@users.noreply.github.com>
2024-06-13 04:48:44 +0000

    Date intro

---
+++ num_math.js
@@ -0,0 +1,18 @@
+// const hundreads =100000
+// //console.log(hundreads.toLocaleString('en-IN'));
+
+// console.log(Math.abs(-8));  //-> change negative value to positive
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+// console.log(Math.min(4,5,8,2));
+// console.log(Math.max(4,5,2,6));
+
+console.log(Math.random()); //--> values in 0 or 1
+console.log((Math.random()*10)+1); 
+console.log(Math.floor(Math.random()*10)+1); 
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random()*(max-min+1))+min);
