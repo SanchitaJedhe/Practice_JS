1692be9be2babee09195ced5d44bc6b5b2990713
Sanchita  Jedhe <144338706+SanchitaJedhe@users.noreply.github.com>
2024-06-15 06:08:18 +0000

    new html

---
+++ Array_demo.js
@@ -0,0 +1,13 @@
+//Arrays
+const myarr=[1,2,8,6,4,3,4] 
+//console.log(myarr[0]);
+
+const arr=new Array(1,5,8,70)
+
+
+//arrays methods
+//myarr.push(3);
+//console.log(myarr.pop(1));
+//console.log(myarr.shift());
+myarr.shift();
+console.log(myarr.join());
--- DateIn.js
+++ DateIn.js
@@ -1,2 +1,11 @@
-let mydate = new Date();
-console.log(muDte) 
+//let myDate = new Date(2023,0,2)
+// console.log(myDate.toString()); // Mon jan 2023 00:00:00
+// console.log(myDate.toDateString()); // MOn jan 2023
+// console.log(myDate.toLocaleString()); //1/2/2023
+// console.log(typeof myDate); //object
+
+let newdate = new Date();
+console.log(newdate);
+console.log(newdate.getMonth()+1);
+console.log(newdate.getDay()); //return 0-6
+ 
